2e9b04ce3c708cfdb4f4bb5a8e4eeb8e0513cf3a
saleemahamad <saleemahamad.uk@gmail.com>
2019-09-25 16:24:23 +0530

    added heloworld program

---
+++ SpringBootExample.java
@@ -0,0 +1,17 @@
+package com.javatpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExample {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootExample.class, args);
+		System.out.println("Welcome to spring boot maven project");
+		
+
+	}
+
+}
